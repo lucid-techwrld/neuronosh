1ef86e8df463c9ddd94cdcb87d8ee83d1b640df1
lucid-techwrld <davisredtiger@gmail.com>
2025-06-05 02:54:35 -0700

    change user import from User to user

--- backend/contollers/AuthController.js
+++ backend/contollers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { createOTP } = require("../helper/otp");
 const { generateJwtToken, setAuthCookie } = require("../helper/token");
